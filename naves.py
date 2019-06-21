ac9595d8a7f8ed7cd1dc1c4736e632f9c4fb5f83
Felipe Caminero <supermegafelipe@gmail.com>
2019-06-21 11:56:55 -0300

    Funcion agregada(PD:costo mucho jaja), y tiempo para moverse agregado

--- naves.py
+++ naves.py
@@ -1,5 +1,4 @@
 import microbit
-
 import random
 
 punto1 = microbit.Image(
@@ -42,20 +41,24 @@ punto5 = microbit.Image(
     "00003"
 )
 
-lugar = random.randint(1, 5)
-
+lugar = random.randint(0, 4)
 puntos = [punto1, punto2, puntomedio, punto4, punto5]
+
+def mostrar_nave(lugar):
+    microbit.display.show(puntos[lugar])
 
 while True:
     if microbit.button_b.was_pressed():
-        if lugar < 5:
+        if lugar < 4:
             lugar = lugar + 1
+            microbit.sleep(300)
 
     elif microbit.button_a.was_pressed():
-        if lugar > 1:
+        if lugar > 0:
             lugar = lugar - 1
+            microbit.sleep(300)
 
-    microbit.display.show(puntos[lugar - 1])
+    mostrar_nave(lugar)
 
     
     
