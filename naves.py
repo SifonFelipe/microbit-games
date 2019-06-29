68b571b79103a68f2fdb0613cc5c0c5e65bd5c11
Felipe Caminero <supermegafelipe@gmail.com>
2019-06-29 19:35:11 -0300

    Funcion agregada, mas facil

--- naves.py
+++ naves.py
@@ -1,51 +1,16 @@
 import microbit
 import random
 
-punto1 = microbit.Image(
-    "00000:"
-    "00000:"
-    "00000:"
-    "00000:"
-    "30000"
-)
-
-punto2 = microbit.Image(
-    "00000:"
-    "00000:"
-    "00000:"
-    "00000:"
-    "03000"
-)
-
-puntomedio = microbit.Image(
-    "00000:"
-    "00000:"
-    "00000:"
-    "00000:"
-    "00300"
-)
-
-punto4 = microbit.Image(
-    "00000:"
-    "00000:"
-    "00000:"
-    "00000:"
-    "00030"
-)
-
-punto5 = microbit.Image(
-    "00000:"
-    "00000:"
-    "00000:"
-    "00000:"
-    "00003"
-)
-
 lugar = random.randint(0, 4)
-puntos = [punto1, punto2, puntomedio, punto4, punto5]
 
-def mostrar_nave(lugar):
-    microbit.display.show(puntos[lugar])
+def armar_imagen(posicion_del_uno):
+    texto = "00000:00000:00000:00000:"
+    for posicion_actual in range(5):
+        if posicion_actual == posicion_del_uno:
+            texto = texto + "1"
+        else:
+            texto = texto + "0"            
+    return microbit.Image(texto)
 
 while True:
     if microbit.button_b.was_pressed():
@@ -58,7 +23,7 @@ while True:
             lugar = lugar - 1
             microbit.sleep(300)
 
-    mostrar_nave(lugar)
+    microbit.display.show(armar_imagen(lugar))
 
     
     
